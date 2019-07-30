7bea4b9ac798ac2a8646442e29a8babf16a4f6f0
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-07-30 13:43:19 -0400

    Fetches useful youtube API data

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,7 @@ async function fetchData() {
     // const search = prompt('Search for: ');
     const search = 'Grand Blue';
     // encodes search query into URL
-    const encodedSearch = encodeURI(search)
+    const encodedSearch = encodeURI(search);
     // input is added to actual url
     const animeSearchURL = `https://api.jikan.moe/v3/search/anime?q=${encodedSearch}`;
 
@@ -23,19 +23,26 @@ async function fetchData() {
     const fetchedAnimeLongData = await fetch(animeURL);
     jsonifiedAnimeLongData = await fetchedAnimeLongData.json();
     console.log(jsonifiedAnimeLongData);
+    const openingList = jsonifiedAnimeLongData.opening_themes;
+    const endingList = jsonifiedAnimeLongData.ending_themes;
+    console.log(openingList);
+    console.log(endingList);
 
     const youtubeKey = 'AIzaSyAG-LdE_3FYJCgVxh1T6RdWbgx8SCpSuKg';
-    youtubeURL = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${youtubeKey}&part=snippet,contentDetails,statistics,status`;
+    // const youtubeURL = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${youtubeKey}&part=snippet,contentDetails,statistics,status`;
 
-    const fetchedYoutubeData = await fetch(youtubeURL);
-    const jsonifiedYoutubeData = await fetchedYoutubeData.json();
-    console.log(jsonifiedYoutubeData);
-
+    async function searchThemes(item) {
+        const youtubeURL = `https://www.googleapis.com/youtube/v3/search?part=id,snippet&type=video&key=${youtubeKey}&q=${item}`;
+        const fetchedYoutubeData = await fetch(youtubeURL);
+        const jsonifiedYoutubeData = await fetchedYoutubeData.json();
+        console.log(jsonifiedYoutubeData);
+        console.log(jsonifiedYoutubeData.items[0].id.videoId);
+        console.log(`https://www.youtube.com/watch?v=${jsonifiedYoutubeData.items[0].id.videoId}`);
+    }
     
-
-
-
-
+    openingList.forEach(item => searchThemes(item));
+    endingList.forEach(item => searchThemes(item));
+    
 
     
     // create page that draws all data
