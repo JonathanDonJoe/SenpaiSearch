a280436b397766751c907b21793e2abde917b652
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-07-30 15:19:22 -0400

    reorganized to functions for repeat use

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const { log } = console;
+const youtubeAPIKey = 'AIzaSyAG-LdE_3FYJCgVxh1T6RdWbgx8SCpSuKg';
 
 // create new entry
 function createCard(el) {
@@ -37,44 +38,63 @@ function createPage() {
 }
 
 
-async function fetchData() {
-    // user input for desired anime themes
-    // const search = prompt('Search for: ');
-    const search = 'Grand Blue';
+// gets the short anime object
+async function getAnimeShortData(search) {
     // encodes search query into URL
     const encodedSearch = encodeURI(search);
     // input is added to actual url
     const animeSearchURL = `https://api.jikan.moe/v3/search/anime?q=${encodedSearch}`;
-
-    //fetch and JSON
+    //fetch and return JSON
     const fetchedAnimeShortData = await fetch(animeSearchURL);
-    jsonifiedAnimeShortData = await fetchedAnimeShortData.json();
-    const malID = jsonifiedAnimeShortData.results[0].mal_id
-    const animeURL = `https://api.jikan.moe/v3/anime/${malID}/`
+    return await fetchedAnimeShortData.json();
+}
+
+// Queries for the long data from the short data
+async function getAnimeLongData(shortData) {
+    const malID = shortData.results[0].mal_id;
+    const animeURL = `https://api.jikan.moe/v3/anime/${malID}/`;
     const fetchedAnimeLongData = await fetch(animeURL);
-    jsonifiedAnimeLongData = await fetchedAnimeLongData.json();
-    console.log(jsonifiedAnimeLongData);
+    return await fetchedAnimeLongData.json();
+}
+
+async function searchThemes(item) {
+    // Queries for the item
+    const youtubeURL = `https://www.googleapis.com/youtube/v3/search?part=id,snippet&type=video&key=${youtubeAPIKey}&q=${item}`;
+    const fetchedYoutubeData = await fetch(youtubeURL);
+    const jsonifiedYoutubeData = await fetchedYoutubeData.json();
+
+    // if no search result hits, ignore it.
+    if (!jsonifiedYoutubeData.items.length) {
+        console.log('Search found no results')
+        return
+    }
+    // else it logs
+    console.log(jsonifiedYoutubeData);
+    console.log(jsonifiedYoutubeData.items[0].id.videoId);
+    console.log(`https://www.youtube.com/watch?v=${jsonifiedYoutubeData.items[0].id.videoId}`);
+
+    // return jsonifiedYoutubeData;
+}
+
+
+async function main() {
+    // user input for desired anime themes
+    // const search = prompt('Search for: ');
+    const search = 'Grand Blue';
+    
+    // Queries the short and long anime data
+    const jsonifiedAnimeShortData = await getAnimeShortData(search);
+    const jsonifiedAnimeLongData = await getAnimeLongData(jsonifiedAnimeShortData);
+    
+    // indexes the OP/ED lists
     const openingList = jsonifiedAnimeLongData.opening_themes;
     const endingList = jsonifiedAnimeLongData.ending_themes;
     console.log(openingList);
     console.log(endingList);
-
-    const youtubeKey = 'AIzaSyAG-LdE_3FYJCgVxh1T6RdWbgx8SCpSuKg';
-    // const youtubeURL = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${youtubeKey}&part=snippet,contentDetails,statistics,status`;
-
-    async function searchThemes(item) {
-        const youtubeURL = `https://www.googleapis.com/youtube/v3/search?part=id,snippet&type=video&key=${youtubeKey}&q=${item}`;
-        const fetchedYoutubeData = await fetch(youtubeURL);
-        const jsonifiedYoutubeData = await fetchedYoutubeData.json();
-        console.log(jsonifiedYoutubeData);
-        console.log(jsonifiedYoutubeData.items[0].id.videoId);
-        console.log(`https://www.youtube.com/watch?v=${jsonifiedYoutubeData.items[0].id.videoId}`);
-    }
     
+    // Search youtube for each OP/ED
     openingList.forEach(item => searchThemes(item));
     endingList.forEach(item => searchThemes(item));
-    
-
     
     // create page that draws all data
     createPage();
@@ -83,4 +103,4 @@ async function fetchData() {
 
 
 
-fetchData();
+main();
